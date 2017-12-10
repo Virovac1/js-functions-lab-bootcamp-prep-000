04bc639e07effd88fb56586a658d0ebf22a1f00a
William Spangler <bspangs@hotmail.com>
2017-12-10 21:33:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,4 @@ function happyHolidayTo(holiday,name) {
 function holidayCountdown (holiday,days){
   return `It's ${days} days until ${holiday}!`;
 }
+//forgot for a moment that ${} means order doesn't matter. Bit confusing had to have (holidays, days)
